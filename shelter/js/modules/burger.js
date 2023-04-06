2295111be0ffcf26716e68c062e8f831c147df2b
Andrew <andrew.suponev@gmail.com>
2023-04-06 22:26:08 +0400

    fix: refactor code for burger

--- shelter/js/modules/burger.js
+++ shelter/js/modules/burger.js
@@ -3,23 +3,23 @@ const burger = () => {
   const nav = document.querySelector('nav');
   const navItems = document.querySelectorAll('.nav__item');
   const body = document.querySelector('body');
-  const shadowBurger = document.querySelector('.shadow_burger');
+  const shadow = document.querySelector('.shadow');
   
   function onClickBurger() {
     nav.classList.toggle('open');
     burger.classList.toggle('active');
     body.classList.toggle('hidden');
-    shadowBurger.classList.toggle('enabled');
+    shadow.classList.toggle('enabled');
   }
   
   burger.addEventListener('click', onClickBurger);
-  shadowBurger.addEventListener('click', onClickBurger);
+  shadow.addEventListener('click', onClickBurger);
   
   for (let navItem of navItems) {
     navItem.addEventListener('click', () => {
       nav.classList.remove('open');
       burger.classList.remove('active');
-      shadowBurger.classList.remove('enabled');
+      shadow.classList.remove('enabled');
       body.classList.remove('hidden');
     });
   }

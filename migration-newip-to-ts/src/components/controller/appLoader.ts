2afec9076a2d350fd45680b049cc40e64a65de89
Andrew <andrew.suponev@gmail.com>
2023-06-12 12:23:41 +0400

    fix: add api url for production

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -1,6 +1,8 @@
 import Loader from './loader';
 
-const BASE_URL: string = process.env.NEWS_BASE_URL ? process.env.NEWS_BASE_URL : 'https://newsapi.org/v2/';
+const BASE_URL: string =
+  process.env.NODE_ENV === 'development' ? 'https://newsapi.org/v2/' : 'https://news-proxy.spanb4.shop/';
+
 const apiKey: string = process.env.NEWS_API_KEY ? process.env.NEWS_API_KEY : '';
 
 class AppLoader extends Loader {

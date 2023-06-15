b4df6fd4bcc921203e2e943f6fa59de5d0d967dd
Andrew <andrew.suponev@gmail.com>
2023-06-15 11:36:52 +0400

    refactor: split method start() into separate private methods in app.ts

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -4,6 +4,7 @@ import AppView from '../view/appView';
 class App {
   private controller: AppController;
   private view: AppView;
+  private isOpenMobileSourses = false;
 
   constructor() {
     this.controller = new AppController();
@@ -13,47 +14,55 @@ class App {
   start(): void {
     const sourcesBlock: HTMLDivElement | null = document.querySelector('.sources');
     const sourcesContent: HTMLDivElement | null = document.querySelector('.sources__content');
-    const selectCategory: HTMLElement | null = document.querySelector('#selectCategory');
+    const selectElementCategory: HTMLSelectElement | null = document.querySelector('#selectCategory');
     const resetBtn: HTMLButtonElement | null = document.querySelector('#resetBtn');
     const menuBtn: HTMLButtonElement | null = document.querySelector('#menuBtn');
+    const closeBtn: HTMLButtonElement | null = document.querySelector('#closeBtn');
 
-    let isOpenMobileSourses = false;
+    this.controller.getSources((data) => this.view.drawFilters(data.sources));
+    this.controller.getSources((data) => this.view.drawSources(data.sources));
+    selectElementCategory?.addEventListener('change', (e) => this.drawFilteredSources(e));
+    sourcesContent?.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+    resetBtn?.addEventListener('click', () => this.resetSelectedCategory(selectElementCategory));
+    menuBtn?.addEventListener('click', () => this.onOpenMobileSources(sourcesBlock, menuBtn));
+    closeBtn?.addEventListener('click', () => this.onCloseMobileSources(sourcesBlock, menuBtn));
+  }
 
-    if (sourcesContent && selectCategory) {
-      this.controller.getSources((data) => this.view.drawFilters(data.sources));
-      this.controller.getSources((data) => this.view.drawSources(data.sources));
-      selectCategory.addEventListener('change', (e) => {
-        const selectedCategory = (e.target as HTMLSelectElement).value;
-        if (selectedCategory) {
-          this.view.clearSources();
-          this.controller.getSources((data) => {
-            const values = data.sources.filter((item) => item.category === selectedCategory);
-            this.view.drawSources(values);
-          });
-        }
-      });
-      sourcesContent.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-    }
-
-    if (resetBtn && selectCategory) {
-      resetBtn.addEventListener('click', () => {
-        (selectCategory as HTMLSelectElement).value = '';
+  private drawFilteredSources(e: Event): void {
+    let selectedCategory: string;
+    if (e.target instanceof HTMLSelectElement) {
+      selectedCategory = e.target.value;
+      if (selectedCategory) {
         this.view.clearSources();
-        this.controller.getSources((data) => this.view.drawSources(data.sources));
-      });
+        this.controller.getSources((data) => {
+          const values = data.sources.filter((item) => item.category === selectedCategory);
+          this.view.drawSources(values);
+        });
+      }
     }
+  }
 
-    if (sourcesBlock) {
-      menuBtn?.addEventListener('click', () => {
-        if (isOpenMobileSourses) {
-          sourcesBlock.style.right = '0';
-          menuBtn.textContent = 'CLOSE SOURCES';
-        } else {
-          sourcesBlock.removeAttribute('style');
-          menuBtn.textContent = 'OPEN SOURCES';
-        }
-        isOpenMobileSourses = !isOpenMobileSourses;
-      });
+  private resetSelectedCategory(selector: HTMLSelectElement | null): void {
+    if (selector) {
+      selector.value = '';
+      this.view.clearSources();
+      this.controller.getSources((data) => this.view.drawSources(data.sources));
+    }
+  }
+
+  private onOpenMobileSources(selectorSources: HTMLDivElement | null, menuBtn: HTMLButtonElement | null): void {
+    if (!this.isOpenMobileSourses) {
+      selectorSources?.classList.add('open');
+      menuBtn?.classList.add('hidden');
+      this.isOpenMobileSourses = true;
+    }
+  }
+
+  private onCloseMobileSources(selectorSources: HTMLDivElement | null, menuBtn: HTMLButtonElement | null): void {
+    if (this.isOpenMobileSourses) {
+      selectorSources?.classList.remove('open');
+      menuBtn?.classList.remove('hidden');
+      this.isOpenMobileSourses = false;
     }
   }
 }

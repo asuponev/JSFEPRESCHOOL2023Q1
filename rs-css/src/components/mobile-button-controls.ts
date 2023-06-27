caedf6232763a68ec47278c49ecb1dfd5244fe67
Andrew <andrew.suponev@gmail.com>
2023-06-27 11:28:11 +0400

    refactor: move dom elements assignment to constructor

--- rs-css/src/components/mobile-button-controls.ts
+++ rs-css/src/components/mobile-button-controls.ts
@@ -1,18 +1,24 @@
+import { BTN_CLOSE, BTN_OPEN, SIDEBAR } from '../constants/elements';
+
 export default class MobileButtonControls {
-  constructor(
-    private sidebar: HTMLElement,
-    private btnOpenLevels: HTMLElement,
-    private btnCloseLevels: HTMLElement
-  ) {
-    this.btnOpenLevels.addEventListener('click', () => this.onOpen());
-    this.btnCloseLevels.addEventListener('click', () => this.onClose());
+  private sidebar: HTMLDivElement | null;
+  private btnOpenLevels: HTMLButtonElement | null;
+  private btnCloseLevels: HTMLButtonElement | null;
+
+  constructor() {
+    this.sidebar = SIDEBAR;
+    this.btnOpenLevels = BTN_OPEN;
+    this.btnCloseLevels = BTN_CLOSE;
+
+    this.btnOpenLevels?.addEventListener('click', () => this.onOpen());
+    this.btnCloseLevels?.addEventListener('click', () => this.onClose());
   }
 
   private onOpen() {
-    this.sidebar.classList.add('open');
+    this.sidebar?.classList.add('open');
   }
 
   private onClose() {
-    this.sidebar.classList.remove('open');
+    this.sidebar?.classList.remove('open');
   }
 }

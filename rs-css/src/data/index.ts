3070edc4a4df16eba332112042fb6f318aebe861
Andrew <andrew.suponev@gmail.com>
2023-06-28 12:53:56 +0400

    feat: add new levels

--- rs-css/src/data/index.ts
+++ rs-css/src/data/index.ts
@@ -16,7 +16,7 @@ const levels: ILevel[] = [
       '  <circle />',
       '</div>',
     ],
-    answer: 'circle',
+    answers: ['circle'],
   },
   {
     id: 2,
@@ -33,7 +33,7 @@ const levels: ILevel[] = [
       '  <circle />',
       '</div>',
     ],
-    answer: '.red',
+    answers: ['.red'],
   },
   {
     id: 3,
@@ -54,7 +54,247 @@ const levels: ILevel[] = [
       '  <circle id="green" />',
       '</div>',
     ],
-    answer: '#green',
+    answers: ['#green', 'circle#green'],
+  },
+  {
+    id: 4,
+    title: 'Select by context',
+    html: `
+      <circle></circle>
+      <square>
+        <circle class="animate"></circle>
+      </square>
+      <circle></circle>
+      <square>
+        <circle class="animate"></circle>
+      </square>
+      <square>
+        <circle class="animate"></circle>
+      </square>
+    `,
+    markup: [
+      '<div class="table">',
+      '  <circle class="red" />',
+      '  <square>',
+      '    <circle class="red" />',
+      '  </square>',
+      '  <circle class="red" />',
+      '  <square class="blue">',
+      '    <circle id="red" />',
+      '  </square>',
+      '  <square>',
+      '    <circle class="red" />',
+      '  </square>',
+      '</div>',
+    ],
+    answers: ['square circle'],
+  },
+  {
+    id: 5,
+    title: 'Select a neighbor',
+    html: `
+      <circle></circle>
+      <square>
+        <circle></circle>
+      </square>
+      <circle></circle>
+      <square class="animate"></square>
+      <square>
+        <circle></circle>
+      </square>
+    `,
+    markup: [
+      '<div class="table">',
+      '  <circle class="red" />',
+      '  <square class="blue">',
+      '    <circle class="red" />',
+      '  </square>',
+      '  <circle id="red" />',
+      '  <square class="blue" />',
+      '  <square class="blue">',
+      '    <circle class="red" />',
+      '  </square>',
+      '</div>',
+    ],
+    answers: ['#red + square', '#red+square'],
+  },
+  {
+    id: 6,
+    title: 'Select neighbors',
+    html: `
+    <circle></circle>
+    <square>
+      <circle></circle>
+    </square>
+    <circle></circle>
+    <square class="animate"></square>
+    <square class="animate">
+      <circle></circle>
+    </square>
+    `,
+    markup: [
+      '<div class="table">',
+      '  <circle class="red" />',
+      '  <square class="blue">',
+      '    <circle class="red" />',
+      '  </square>',
+      '  <circle id="red" />',
+      '  <square class="blue" />',
+      '  <square class="blue">',
+      '    <circle class="red" />',
+      '  </square>',
+      '</div>',
+    ],
+    answers: ['#red ~ square', '#red~square'],
+  },
+  {
+    id: 7,
+    title: 'Select with NOT',
+    html: `
+    <circle class="animate"></circle>
+    <square>
+      <circle class="animate"></circle>
+    </square>
+    <circle></circle>
+    <square>
+      <circle class="animate"></circle>
+    </square>
+    <square>
+      <circle class="animate"></circle>
+    </square>
+    `,
+    markup: [
+      '<div class="table">',
+      '  <circle />',
+      '  <square class="blue">',
+      '    <circle />',
+      '  </square>',
+      '  <circle id="red" />',
+      '  <square>',
+      '    <circle class="red" />',
+      '  </square>',
+      '  <square>',
+      '    <circle class="red" />',
+      '  </square>',
+      '</div>',
+    ],
+    answers: ['circle:not(#red)'],
+  },
+  {
+    id: 8,
+    title: 'Select by attribute',
+    html: `
+    <circle></circle>
+    <square>
+      <circle class="animate"></circle>
+    </square>
+    <circle class="animate"></circle>
+    <square>
+      <circle></circle>
+    </square>
+    <square>
+      <circle class="animate"></circle>
+    </square>
+    `,
+    markup: [
+      '<div class="table">',
+      '  <circle />',
+      '  <square class="blue">',
+      '    <circle title="hello" />',
+      '  </square>',
+      '  <circle id="red" title="hello" />',
+      '  <square>',
+      '    <circle class="red" />',
+      '  </square>',
+      '  <square>',
+      '    <circle class="red" title="hello" />',
+      '  </square>',
+      '</div>',
+    ],
+    answers: ['circle[title]'],
+  },
+  {
+    id: 9,
+    title: 'Select all colored',
+    html: `
+    <circle class="animate"></circle>
+    <square class="animate">
+      <circle class="animate"></circle>
+    </square>
+    <circle></circle>
+    <square>
+      <circle class="animate"></circle>
+    </square>
+    <square class="animate">
+      <circle class="animate"></circle>
+    </square>
+    `,
+    markup: [
+      '<div class="table">',
+      '  <circle id="red" />',
+      '  <square class="blue">',
+      '    <circle class="red" />',
+      '  </square>',
+      '  <circle id="nocorners" />',
+      '  <square id="example">',
+      '    <circle class="red" />',
+      '  </square>',
+      '  <square id="blue">',
+      '    <circle class="red" />',
+      '  </square>',
+      '</div>',
+    ],
+    answers: [
+      '#red, #blue, .red, .blue',
+      '#red, .red, #blue, .blue',
+      '#red, .blue, .red, #blue',
+      '#red, .red, .blue, #blue',
+      '#blue, .blue, .red, #red',
+      '#blue, .red, .blue, #red',
+      '.blue, .red, #blue, #red',
+      '.red, .blue, #red, #blue',
+      '.red, #red, .blue, #blue',
+      '.blue, #blue, .red, #red',
+    ],
+  },
+  {
+    id: 10,
+    title: 'Select all',
+    html: `
+    <circle class="animate"></circle>
+    <square class="animate">
+      <circle class="animate"></circle>
+    </square>
+    <circle class="animate"></circle>
+    <square class="animate">
+      <circle class="animate"></circle>
+    </square>
+    <square class="animate">
+      <circle class="animate"></circle>
+    </square>
+    `,
+    markup: [
+      '<div class="table">',
+      '  <circle />',
+      '  <square class="blue">',
+      '    <circle />',
+      '  </square>',
+      '  <circle id="red" />',
+      '  <square>',
+      '    <circle class="red" />',
+      '  </square>',
+      '  <square>',
+      '    <circle class="red" />',
+      '  </square>',
+      '</div>',
+    ],
+    answers: [
+      '*',
+      'circle, square',
+      'square, circle',
+      'circle,square',
+      'square,circle',
+    ],
   },
 ];
 

c8ba9fc9a969ecf977ec18865d6cfc14a694aac9
Andrew <andrew.suponev@gmail.com>
2023-07-07 23:18:01 +0400

    feat: implement error handling when server is off

--- async-race/src/api/requests.ts
+++ async-race/src/api/requests.ts
@@ -2,12 +2,18 @@ import endpoints from '../constants/url';
 import { ICar } from '../types/types';
 
 export const getAllCars = async (page: number, limit = 7) => {
-  const response = await fetch(
-    `${endpoints.garage}?_page=${page}&_limit=${limit}`
-  );
+  try {
+    const response = await fetch(
+      `${endpoints.garage}?_page=${page}&_limit=${limit}`
+    );
 
-  const data: ICar[] = await response.json();
-  return data;
+    const data: ICar[] = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(
+      'Something went wrong. Most likely, you need to run the server locally from this repo: https://github.com/mikhama/async-race-api'
+    );
+  }
 };
 
 export const getCarById = async (id: number) => {
--- async-race/src/components/garage/garage.ts
+++ async-race/src/components/garage/garage.ts
@@ -7,26 +7,40 @@ import baseSectionTitle from '../base/sectionTitle/sectionTitle';
 import './garage.scss';
 
 const garagePage = 1;
-const cars: ICar[] = await getAllCars(garagePage);
+let cars: ICar[] | undefined;
+let errorText: string;
+
+try {
+  cars = await getAllCars(garagePage);
+} catch (error) {
+  errorText = error as string;
+}
 
 const garageView = (): HTMLElement => {
   const garage = document.createElement('section');
   garage.classList.add('section', 'garage');
 
-  const sectionTitle = baseSectionTitle({ text: 'Garage', count: cars.length });
+  if (cars) {
+    const sectionTitle = baseSectionTitle({
+      text: 'Garage',
+      count: cars.length,
+    });
 
-  const paginationTitle = basePaginationTitle({
-    text: 'Page',
-    page: garagePage,
-  });
+    const paginationTitle = basePaginationTitle({
+      text: 'Page',
+      page: garagePage,
+    });
 
-  const garageItems = document.createElement('div');
-  garageItems.classList.add('garage__items');
-  garageItems.append(...cars.map((car) => carView(car)));
+    const garageItems = document.createElement('div');
+    garageItems.classList.add('garage__items');
+    garageItems.append(...cars.map((car) => carView(car)));
 
-  const paginationBtns = basePaginationBtns();
+    const paginationBtns = basePaginationBtns();
 
-  garage.append(sectionTitle, paginationTitle, garageItems, paginationBtns);
+    garage.append(sectionTitle, paginationTitle, garageItems, paginationBtns);
+  } else {
+    garage.textContent = errorText;
+  }
 
   return garage;
 };

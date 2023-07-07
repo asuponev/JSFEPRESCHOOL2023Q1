6d4a481d4097d68291d5a218d8523a819f4755bc
Andrew <andrew.suponev@gmail.com>
2023-07-07 22:39:40 +0400

    feat: implement car item component

---
+++ async-race/src/components/car/car.ts
@@ -0,0 +1,49 @@
+import './car.scss';
+import { ICar } from '../../types/types';
+import uiButton from '../ui/button/button';
+import carIconView from './car-icon/car-icon';
+
+const carView = (props: ICar) => {
+  const car = document.createElement('div');
+  car.classList.add('car');
+  // create buttons for edit and delete car
+  const carButtons = document.createElement('div');
+  carButtons.classList.add('car__buttons');
+  const btnSelect = uiButton({ text: 'select', customClass: 'button--minor' });
+  const btnDelete = uiButton({ text: 'delete', customClass: 'button--minor' });
+  // create title car element
+  const carTitle = document.createElement('p');
+  carTitle.textContent = props.name;
+  carButtons.append(btnSelect, btnDelete, carTitle);
+  // create road element
+  const carRoad = document.createElement('div');
+  carRoad.classList.add('car__road');
+  // create buttons for motion control
+  const roadButtons = document.createElement('div');
+  roadButtons.classList.add('car__buttons');
+  const btnDrive = uiButton({
+    text: 'D',
+    customClass: 'button--drive',
+    disabled: false,
+  });
+  const btnStop = uiButton({
+    text: 'R',
+    customClass: 'button--stop',
+    disabled: true,
+  });
+  roadButtons.append(btnDrive, btnStop);
+  // create car icon
+  const roadCarIcon = carIconView(props.color);
+  // create finish element
+  const roadFinishIcon = document.createElement('div');
+  roadFinishIcon.classList.add('car__road__finish');
+  roadFinishIcon.textContent = 'FINISH';
+
+  carRoad.append(roadButtons, roadCarIcon, roadFinishIcon);
+
+  car.append(carButtons, carRoad);
+
+  return car;
+};
+
+export default carView;

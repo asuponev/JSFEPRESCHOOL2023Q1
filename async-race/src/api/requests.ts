b161dc461ad27c931679442ffe0562f59f6b1103
Andrew <andrew.suponev@gmail.com>
2023-07-08 20:22:04 +0400

    feat: implement func getWinners

--- async-race/src/api/requests.ts
+++ async-race/src/api/requests.ts
@@ -1,5 +1,5 @@
 import endpoints from '../constants/url';
-import { ICar } from '../types/types';
+import { ICar, IWinner } from '../types/types';
 
 export const getAllCars = async (page: number, limit = 7) => {
   try {
@@ -21,4 +21,19 @@ export const getCarById = async (id: number) => {
 
   const data: ICar = await response.json();
   return data;
+};
+
+export const getWinners = async (page: number, limit = 10) => {
+  try {
+    const response = await fetch(
+      `${endpoints.winners}?_page=${page}&_limit=${limit}`
+    );
+
+    const data: IWinner[] = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(
+      'Something went wrong. Most likely, you need to run the server locally from this repo: https://github.com/mikhama/async-race-api'
+    );
+  }
 };

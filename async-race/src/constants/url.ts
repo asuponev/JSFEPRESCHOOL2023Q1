1fa887547533e3211386c3a12a2ded5c09c543f9
Andrew <andrew.suponev@gmail.com>
2023-07-10 18:51:14 +0400

    feat: add engine endpoint

--- async-race/src/constants/url.ts
+++ async-race/src/constants/url.ts
@@ -1,6 +1,7 @@
 enum Endpoint {
   garage = 'garage',
   winners = 'winners',
+  engine = 'engine',
 }
 
 const baseUrl = 'http://localhost:3000';
@@ -8,4 +9,5 @@ const baseUrl = 'http://localhost:3000';
 export default {
   garage: `${baseUrl}/${Endpoint.garage}`,
   winners: `${baseUrl}/${Endpoint.winners}`,
+  engine: `${baseUrl}/${Endpoint.engine}`,
 };

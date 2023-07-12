48e16c5617c0bfbfd26fdd36d3eed5d86aeda8f7
Andrew <andrew.suponev@gmail.com>
2023-07-12 23:51:44 +0400

    fix: state saves items with a unique id

--- async-race/src/state/htmlState.ts
+++ async-race/src/state/htmlState.ts
@@ -13,9 +13,15 @@ const htmlState: IHtmlState = {
   elements: [],
   currentSelectCar: null,
   addToElements: (id: string, element: HTMLElements): void => {
-    htmlState.elements.push({ id, element });
+    const index = htmlState.elements.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      htmlState.elements[index] = { id, element };
+    } else {
+      htmlState.elements.push({ id, element });
+    }
   },
   removeFromElements: (id: string): void => {
+    htmlState.elements.find((el) => el.id === id)?.element.remove();
     htmlState.elements = htmlState.elements.filter((item) => item.id !== id);
   },
   getElementById: (elementId: string): HTMLElements | undefined => {

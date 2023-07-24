7ed3e09e525a69cdf1a7adcbb66f8b4eb83e7a21
Andrew <andrew.suponev@gmail.com>
2023-07-25 00:52:23 +0400

    fix: call getWinner, updateWinner, createWinner in try...catch

--- async-race/src/actions/onCreateWinner.ts
+++ async-race/src/actions/onCreateWinner.ts
@@ -3,16 +3,23 @@ import winnersStore from '../store/winnersStore';
 import { IWinner } from '../types/types';
 
 const onCreateWinner = async (winner: Omit<IWinner, 'wins'>): Promise<void> => {
-  const oldWinner = await getWinner(winner.id);
-  if (oldWinner.id) {
-    const { id, wins, time } = oldWinner;
-    const newTime = time <= winner.time ? time : winner.time;
-    const updatedWinner = { id, wins: wins + 1, time: newTime };
-    await updateWinner(updatedWinner);
-    winnersStore.dispatch({ type: 'UPDATE_WINNER', payload: [updatedWinner] });
-  } else {
-    const newWinner = await createWinner({ ...winner, wins: 1 });
-    winnersStore.dispatch({ type: 'ADD_WINNER', payload: [newWinner] });
+  try {
+    const oldWinner = await getWinner(winner.id);
+    if (oldWinner.id) {
+      const { id, wins, time } = oldWinner;
+      const newTime = time <= winner.time ? time : winner.time;
+      const updatedWinner = { id, wins: wins + 1, time: newTime };
+      await updateWinner(updatedWinner);
+      winnersStore.dispatch({
+        type: 'UPDATE_WINNER',
+        payload: [updatedWinner],
+      });
+    } else {
+      const newWinner = await createWinner({ ...winner, wins: 1 });
+      winnersStore.dispatch({ type: 'ADD_WINNER', payload: [newWinner] });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 

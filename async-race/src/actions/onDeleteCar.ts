d26fc840482331701e5a0343ed427e9c1a63ee20
Andrew <andrew.suponev@gmail.com>
2023-07-25 01:17:46 +0400

    fix: off race mode when all animate cars deleted

--- async-race/src/actions/onDeleteCar.ts
+++ async-race/src/actions/onDeleteCar.ts
@@ -5,6 +5,7 @@ import winnersStore from '../store/winnersStore';
 import fetchWinners from './fetchWinners';
 import { ICar } from '../types/types';
 import fetchCars from './fetchCars';
+import moveState from '../store/moveState';
 
 const onDeleteCar = async (car: ICar): Promise<void> => {
   if (window.confirm(`are you sure you want to remove "${car.name}"?`)) {
@@ -13,6 +14,14 @@ const onDeleteCar = async (car: ICar): Promise<void> => {
       await fetchCars();
       // carStore.dispatch({ type: 'REMOVE_ITEM', payload: [car] });
       carStore.dispatch({ type: 'RESET_UPDATE_CAR' });
+
+      moveState.removeStoppedEngine(car.id);
+      moveState.stopAnimation(`animate-car-icon-${car.id}`);
+      moveState.removeAnimation(`animate-car-icon-${car.id}`);
+      if (moveState.startedEngine.length === 0) {
+        carStore.dispatch({ type: 'OFF_RACE_MODE' });
+      }
+
       const isWinner = await getWinner(car.id);
       if (isWinner.id) {
         await deleteWinner(car.id);
